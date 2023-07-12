49acc717c7c5e3024a3f915e1679620f5c768deb
heexohee <rsoy2918@naver.com>
2023-07-13 00:32:43 +0900

    json-test-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log();
+        console.log("response.json()", response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log("json", json);
+        setData([...json]);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((item) => {
+        return (
+          <div
+            style={{
+              border: "1px solid black",
+              margin: "3px",
+            }}
+            key={item.id}
+          >
+            <ul>
+              <li>userId : {item.userId}</li>
+              <li>id : {item.id}</li>
+              <li>title : {item.title}</li>
+              <li>body : {item.body}</li>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
